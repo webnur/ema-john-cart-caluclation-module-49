6a3d68ddde11e5c01023e7dcaacd1e7dfda25386
webnur <mdnuruli291@gmai.com>
2022-10-07 20:49:49 +0600

    order page designed and Remove Item handler and Make Data consistent across UI done alhamdulillah

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,15 +1,25 @@
 import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
+import { removeFromDb } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
+import ReviewItem from '../ReviewItem/ReviewItem';
 
 const Orders = () => {
     const {products, initialCart} = useLoaderData();
-    const [cart, setCart] = useState(initialCart)
+    const [cart, setCart] = useState(initialCart);
+
+    const handleRemoveItem = (id) => {
+        const remaining = cart.filter(product => product.id !== id);
+        setCart(remaining);
+        removeFromDb(id)
+    }
 
     return (
         <div className='shop-container'>
-           <div className='products-container'>
-
+           <div className='orders-container'>
+                {
+                    cart.map(product => <ReviewItem key={product.id} product={product} handleRemoveItem={handleRemoveItem}></ReviewItem>)
+                }
            </div>
            <div className='cart-container'>
                 <Cart cart={cart}></Cart>
